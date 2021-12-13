f11bab6878b9be3ada03da6e47c6844419f4e663
Rees5 <reesalumasa@gmail.com>
2021-12-13 20:39:04 +0300

    Add extra resources

--- RainbowBlast/Splash.h
+++ RainbowBlast/Splash.h
@@ -3,6 +3,15 @@
 #include "GLFW/glfw3.h"
 class Splash
 {
+	public:
+		// settings
+		const unsigned int SCR_WIDTH = 800;
+		const unsigned int SCR_HEIGHT = 600;
+		//methods
+		int display(); //displays splah screen
+		//void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+		//void processInput(GLFWwindow* window);
+
 
 };
 
