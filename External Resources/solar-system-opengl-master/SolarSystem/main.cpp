4cb34fc286cc52f29def0f5a02563fe7a21e5ad9
Andesia <lihasigrace@gmail.com>
2022-01-28 08:44:54 +0300

    update

---
+++ External Resources/solar-system-opengl-master/SolarSystem/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  SolarSystem
+//
+//  Created by chan on 4/23/14.
+//  Copyright (c) 2014 chan. All rights reserved.
+//
+
+#include <iostream>
+#include "window_controller.h"
+
+int main(int argc, char **argv) {
+    driver::run(argc, argv);
+    return 0;
+}
+
